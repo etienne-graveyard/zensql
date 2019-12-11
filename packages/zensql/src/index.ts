a7c9d643f1d04d3ffafc57bfb32f134f98d90435
Etienne Dldc <e.deladonchamps@gmail.com>
2019-12-11 19:17:15 +0100

    Fix zensql

--- packages/zensql/src/index.ts
+++ packages/zensql/src/index.ts
@@ -1,11 +1,2 @@
-import * as zenslqGenerate from '@zensql/scripts';
-export { Tables } from '@zensql/scripts';
-
-export interface Options {
-  source: string;
-  target: string;
-}
-
-export async function command(argv: Array<string>) {
-  return zenslqGenerate.command(argv);
-}
+export * from '@zensql/scripts';
+export * from '@zensql/builder';
